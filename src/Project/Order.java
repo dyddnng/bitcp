6b23965a84606d20969efebeeaa72c97064c183f
Zagg2732 <zagg2732@gmail.com>
2021-02-23 13:58:53 +0900

    2/23 Seller 클래스 짯음 / 6조 화이팅 ^^!

--- src/Project/Order.java
+++ src/Project/Order.java
@@ -1,13 +1,26 @@
 package Project;
 
 public class Order {
+    private int orderNum; //아니잠간.. orderNum이 정상적으로 누적되려면. I/O를 써야하잖아????
+    private boolean permission;
     private Food food; //
     private String user; // User에서 이름만(ID)
     private String seller; //Seller에서 이름만(ID)
+
+    public int getOrderNum() {
+        return orderNum;
+    }
+
+    public void setPermission(boolean permission) {
+        this.permission = permission;
+    }
+
+
 
     public Order(Food food, String users, String sellers) {
         this.food = food;
         this.user = users;
         this.seller = sellers;
+        this.permission = false;
     }
 }
--- src/Project/Seller.java
+++ src/Project/Seller.java
@@ -1,6 +1,8 @@
 package Project;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /*
 
@@ -14,30 +16,72 @@ import java.util.ArrayList;
 판매자는 가게 Open, Close 를 할 수 있다.
  */
 public class Seller {
-////////////////// 주문확인할때 주문이라는 Order객체가 있어야할것으로 예상됨////////
-    //ArrayList<Order> orders; //주문내역
-    //ArrayList<Order> sells; //판매내역
-
-    //주문확인
+    private boolean open;
+    private String storeName;
+    private ArrayList<Food> menu;
+    private ArrayList<Order> orderList;
+////////주문 내역 확인 역순으로 할까? 그건 다 만든다음에 고민해보기//////////
+    //주문 내역 확인
     public void checkOrders() {
+        // I/O 로 파일 불러옴 파일이름은 storeName과 동일하다
 
+        String baseFileFolder = "C:\\BitEats\\Orders\\SellerOrder"; //기본 경로
+        String filename = baseFileFolder + "\\" + this.storeName + ".txt";
+
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        ObjectInputStream in = null;
+
+        try {
+            fis = new FileInputStream(filename); //파일이름불러오고
+            bis = new BufferedInputStream(fis); //불러온걸 버퍼로옮기고
+            in = new ObjectInputStream(bis); //버퍼로 옮긴걸 조립한다
+
+            Object orders = null;
+
+            while ((orders = in.readObject()) != null) {
+                System.out.println((Order)orders);
+            }
+
+        } catch (FileNotFoundException fn) {
+            System.out.println("파일이 존재하지 않습니다");
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println(cnfe.getMessage());
+        } finally {
+            try {
+                in.close();
+                bis.close();
+                fis.close();
+            } catch (Exception e3) {
+                e3.printStackTrace();
+            }
+        }
     }
     //주문승인 or 거절
-    public void yesOrNo() {
-
+    public void permission(Order o) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(this.storeName + " 주문관리 시스템입니다. \n주문이 들어왔습니다.");
+        System.out.println(o);
+        System.out.println("주문번호 " + o.getOrderNum() + "번을 승인하시려면 1, 취소하시려면 2를 입력해주세요");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1 :
+                System.out.println("주문번호 " + o.getOrderNum() + "번을 승인했습니다.");
+                o.setPermission(true);
+                scanner.close();
+                break;
+            case 2 :
+                System.out.println("주문번호 " + o.getOrderNum() + "번을 거절했습니다.");
+                o = null; //이렇게되나???
+                scanner.close();
+                break;
+        }
     }
-    //배달시간알림
-    public void deliveryTimes() {
 
-    }
     //가게 열기 닫기
-    public boolean onOff() {
-        return true;
+    public boolean open() {
+        return !open;
     }
-
-    //배달료를 판매자가 설정할 수 있게?
-    public void setDeliveryFee() {
-
-    }
-
 }
