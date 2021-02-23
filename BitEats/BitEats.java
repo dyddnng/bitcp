2d9052ec87118ea17e820d87885b68fc890f8b77
K-MyeongHwan <78404952+K-MyeongHwan@users.noreply.github.com>
2021-02-23 20:45:45 +0900

    2월 23일 BitEats

--- BitEats/BitEats.java
+++ BitEats/BitEats.java
@@ -9,11 +9,14 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
-public class BitEats_1 {
+public class BitEats {
+
     private Map<String, String> users;
     private List<Order> ordersList;
     private Scanner scanner;
@@ -22,29 +25,30 @@ public class BitEats_1 {
     private ObjectOutputStream oot;
     private FileInputStream fis;
     private BufferedInputStream bis;
-    private ObjectInputStream oit;
+    private ObjectInputStream ois;
     private final String USERINFO = "C:\\Temp";
+    Seller seller;
+    Customer customer;
 
-    public BitEats_1() {
+    public BitEats() {
         users = new HashMap<String, String>();
         ordersList = new ArrayList<Order>();
         scanner = new Scanner(System.in);
     }
 
     public boolean join() {
-        System.out.println("가입자 유형을 입력해주세요 (소비자:\'1\', 판매자:\'2\'");
-        System.out.println("소비자일 경우 \'S\', 판매자일 경우 \'C\' 가 아이디 맨 앞에 자동으로 붙습니다");
-        System.out.println("> ");
+        System.out.println("가입자 유형을 입력해주세요 (소비자:\'1\', 판매자:\'2\')");
+        System.out.print("> ");
         int type = scanner.nextInt();
 
         System.out.println("가입할 아이디를 입력해주세요 (소문자)");
         System.out.print("> ");
         scanner.nextLine();
-        String id = scanner.nextLine().trim().toLowerCase();
+        String id = scanner.nextLine();
 
         System.out.println("가입할 비밀번호를 입력해주세요");
         System.out.print("> ");
-        String pwd = scanner.nextLine();
+        String pwd = scanner.nextLine().trim();
 
         if (users.containsKey(id) || users.containsValue(pwd)) {
             System.out.println("이미 존재하는 아이디거나 비밀번호입니다.");
@@ -56,14 +60,18 @@ public class BitEats_1 {
             return false;
         } else if (type == 1) {
             try {
-                fos = new FileOutputStream(USERINFO, true);
+                fos = new FileOutputStream("userInfo.txt", true);
                 bos = new BufferedOutputStream(fos);
                 oot = new ObjectOutputStream(bos);// 직렬화 끝
-                //직렬화 대상
-                System.out.println("주소를 입력해 주세요.");
-                String address = scanner.nextLine();
-                oot.writeObject(new Customer(id,"bit시 bit동"));
-                users.put("C" + id, pwd);
+
+                id = "C" + id;
+
+                // 직렬화 대상
+                oot.writeObject(new Customer(id, "bit시 bit동"));
+                System.out.println(id);
+                System.out.println(pwd);
+                users.put(id, pwd);
+
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             } finally {
@@ -79,12 +87,18 @@ public class BitEats_1 {
 
         } else if (type == 2) {
             try {
-                fos = new FileOutputStream(USERINFO, true);
+                fos = new FileOutputStream("userInfo.txt", true);
                 bos = new BufferedOutputStream(fos);
-                oot = new ObjectOutputStream(bos);
-                Seller test = new Seller(id);
-                oot.writeObject(test);// 완성!!
-                users.put("S" + id, pwd);
+                oot = new ObjectOutputStream(bos);// 직렬화 끝
+
+                id = "S" + id;
+
+                // 직렬화 대상
+                oot.writeObject(new Seller(id));
+                System.out.println(id);
+                System.out.println(pwd);
+                users.put(id, pwd);
+
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             } finally {
@@ -97,21 +111,24 @@ public class BitEats_1 {
                 }
 
             }
+
         }
         return true;
     }
 
     public boolean login() {
         boolean isLogin;
+
         System.out.println("아이디를 입력해주세요");
+        System.out.println("소비자일 경우 \'C\', 판매자일 경우 \'S\' 가 아이디 맨 앞에 자동으로 붙었습니다.");
         System.out.print("> ");
         String id = scanner.nextLine();
 
         System.out.println("비밀번호를 입력해주세요");
         System.out.print("> ");
-        String pwd = scanner.nextLine();
+        String pwd = scanner.nextLine().trim();
 
-        if (!users.containsKey(id)) {
+        if (!(users.containsKey(id))) {
             System.out.println("존재하지 않는 아이디입니다. 다시 입력해주세요.");
             isLogin = false;
         } else {
@@ -125,17 +142,19 @@ public class BitEats_1 {
         }
         if (isLogin) {
             try {
-                fis = new FileInputStream(USERINFO);// C:\\BitEats\\User\\Customer\\id.txt
+                fis = new FileInputStream("userInfo.txt");// C:\\BitEats\\User\\Customer\\id.txt
                 bis = new BufferedInputStream(fis);
-                oit = new ObjectInputStream(bis);
+                ois = new ObjectInputStream(bis);
 
-                if(id.charAt(0) == 'C') {
-                    Customer customer = (Customer)oit.readObject();
-                } else if(id.charAt(0) == 'S') {
-                    Seller seller = (Seller)oit.readObject();
+                if (id.charAt(0) == 'C') {
+                    this.customer = (Customer) ois.readObject();
+                } else if (id.charAt(0) == 'S') {
+                    this.seller = (Seller) ois.readObject();
+                    System.out.println("중간점검");
                 } else {
                     System.out.println("??? 어케 왔누");
                 }
+
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             } finally {
@@ -154,6 +173,10 @@ public class BitEats_1 {
     }
 
     public void usersList() {
-
+        Set set = users.entrySet();
+        Iterator it = set.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
     }
 }
